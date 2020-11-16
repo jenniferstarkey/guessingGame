e04d256c8f54e212890cce814affec0b392d2231
jenniferstarkey <jenniferrstarkey@yahoo.com>
2020-11-16 09:40:38 -0600

    console question

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace guessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's the secret number?");
+        }
+    }
+}
